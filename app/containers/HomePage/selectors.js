2025ac429da62d2c58fff57e35a98b7225ebef5a
Imran Kheraj <ikheraj17@gmail.com>
2020-09-09 12:34:47 -0500

    added selectStrings selector to selectors.js

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -7,10 +7,18 @@ import { initialState } from './reducer';
 
 const selectHome = state => state.home || initialState;
 
-const makeSelectUsername = () =>
+const makeSelectUsername = () => {
   createSelector(
     selectHome,
     homeState => homeState.username,
   );
+};
+
+const makeSelectStrings = () => {
+  createSelector(
+    selectHome,
+    homeState => homeState.strings,
+  );
+};
 
 export { selectHome, makeSelectUsername };

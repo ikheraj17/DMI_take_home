d119c41d6b1c60015d1e64d6d01f52d085dafee0
Imran Kheraj <ikheraj17@gmail.com>
2020-09-11 10:03:00 -0500

    deleted unused tests, added tests for String reducer

--- app/containers/String/tests/reducer.test.js
+++ app/containers/String/tests/reducer.test.js
@@ -1,7 +1,7 @@
 import produce from 'immer';
 import stringReducer from '../reducer';
-// import { someAction } from '../actions';
 import { loadStrings, loadStringsSuccess } from '../actions';
+
 /* eslint-disable default-case, no-param-reassign */
 describe('stringReducer', () => {
   let state;
@@ -18,17 +18,28 @@ describe('stringReducer', () => {
     expect(stringReducer(undefined, {})).toEqual(expectedResult);
   });
 
-  /**
-   * Example state change comparison
-   *
-   * it('should handle the someAction action correctly', () => {
-   *   const expectedResult = produce(state, draft => {
-   *     draft.loading = true;
-   *     draft.error = false;
-   *     draft.userData.nested = false;
-   *   });
-   *
-   *   expect(appReducer(state, someAction())).toEqual(expectedResult);
-   * });
-   */
+  it('should handle the loadStrings action correctly', () => {
+    const expectedResult = produce(state, draft => {
+      draft.loading = true;
+      draft.error = false;
+      draft.allStrings = [];
+    });
+
+    expect(stringReducer(state, loadStrings())).toEqual(expectedResult);
+  });
+
+  it('should handle the loadStringsSuccess action correctly', () => {
+    const expectedResult = produce(state, draft => {
+      draft.loading = false;
+      draft.error = false;
+      draft.allStrings = [{ id: 1, text: 'this is a string' }];
+    });
+
+    expect(
+      stringReducer(
+        state,
+        loadStringsSuccess([{ id: 1, text: 'this is a string' }]),
+      ),
+    ).toEqual(expectedResult);
+  });
 });
